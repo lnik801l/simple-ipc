df8c4b394aa400dc1c8dd4ee9a43769da23a4a86
lnik801l <l.nik801.l@gmail.com>
2024-01-21 18:41:53 +0300

    refactor: remove unused code from build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,28 +1,4 @@
-import { esbuildPluginFilePathExtensions } from "esbuild-plugin-file-path-extensions"
-import { replaceTscAliasPaths } from "tsc-alias"
 import { defineConfig } from "tsup"
-
-// // eslint-disable-next-line import/no-default-export
-// export default defineConfig({
-// 	entry: ["src/**/*"],
-// 	clean: true,
-// 	dts: true,
-// 	bundle: true,
-// 	format: ["esm"],
-// 	target: ["node18", "chrome122"],
-// 	outDir: "build",
-
-// 	esbuildPlugins: [esbuildPluginFilePathExtensions()],
-
-// 	async onSuccess() {
-// 		await replaceTscAliasPaths({
-// 			configFile: "tsconfig.json",
-// 			watch: false,
-// 			outDir: "build",
-// 			declarationDir: "build",
-// 		})
-// 	},
-// })
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
