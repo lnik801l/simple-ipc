3da1cf6579c7af6c8b743b1e89137a7b5be1306b
bgenia <xbgenia@gmail.com>
2024-01-21 16:25:04 +0300

    build: test build implementation

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,38 @@
+import { esbuildPluginFilePathExtensions } from "esbuild-plugin-file-path-extensions"
+import { replaceTscAliasPaths } from "tsc-alias"
 import { defineConfig } from "tsup"
+
+// // eslint-disable-next-line import/no-default-export
+// export default defineConfig({
+// 	entry: ["src/**/*"],
+// 	clean: true,
+// 	dts: true,
+// 	bundle: true,
+// 	format: ["esm"],
+// 	target: ["node18", "chrome122"],
+// 	outDir: "build",
+
+// 	esbuildPlugins: [esbuildPluginFilePathExtensions()],
+
+// 	async onSuccess() {
+// 		await replaceTscAliasPaths({
+// 			configFile: "tsconfig.json",
+// 			watch: false,
+// 			outDir: "build",
+// 			declarationDir: "build",
+// 		})
+// 	},
+// })
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
-	entry: ["src/index.ts"],
+	splitting: true,
 	clean: true,
 	dts: true,
 	format: ["cjs", "esm"],
-	target: ["node20"],
+	bundle: true,
+	skipNodeModulesBundle: true,
+	entry: ["src/**/*.ts"],
+	target: ["node18", "chrome122"],
 	outDir: "build",
 })
